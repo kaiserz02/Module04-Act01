b8621bb8d5b57f19a1a4fad6b4bd4ce94478f9e0
kaiserz02 <59811857+kaiserz02@users.noreply.github.com>
2021-09-14 18:31:42 +0800

    Add files via upload

---
+++ NumericValidationBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PDC_Module04_Activity01
+{
+    class NumericValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextTextChanged(object sender, TextChangedEventArgs args)
+        {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+}
+}
